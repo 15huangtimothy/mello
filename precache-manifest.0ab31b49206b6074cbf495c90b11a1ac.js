64cee5928da0880f9e80257509f6a1cd4c1d52c2
Tim <tim@Tims-MacBook-Pro.local>
2019-01-21 13:08:43 -0800

    Updates

---
+++ precache-manifest.0ab31b49206b6074cbf495c90b11a1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c035f84f193ebfd920f",
+    "url": "/mello/static/js/runtime~main.7c035f84.js"
+  },
+  {
+    "revision": "5808c34f4c321e3275b4",
+    "url": "/mello/static/js/main.5808c34f.chunk.js"
+  },
+  {
+    "revision": "66ee20a26a960d41fbce",
+    "url": "/mello/static/js/1.66ee20a2.chunk.js"
+  },
+  {
+    "revision": "5808c34f4c321e3275b4",
+    "url": "/mello/static/css/main.9f38c5a3.chunk.css"
+  },
+  {
+    "revision": "66ee20a26a960d41fbce",
+    "url": "/mello/static/css/1.b9db1d7f.chunk.css"
+  },
+  {
+    "revision": "c1d709dafb6130fe888b9dd5e641746c",
+    "url": "/mello/index.html"
+  }
+];
